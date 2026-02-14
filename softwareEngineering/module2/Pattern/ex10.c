38b8dc0209d83d7ce14220518636ee4523277eb6
Rajvi2020 <250327823+Rajvi2020@users.noreply.github.com>
2026-02-14 10:09:06 +0530

    commit

---
+++ softwareEngineering/module2/Pattern/ex10.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+  int num;
+  printf("enter a number");
+  scanf("%d",&num);
+  for(int i=1;i<=num;i++){
+      for(int space=1;space<=num-i;space++){
+        printf(" ");
+      }
+      for(int j=1;j<=i;j++){
+        printf("*");
+      }
+      printf("\n");
+  }
+
+
+  return 0;
+}
+
---
+++ softwareEngineering/module2/Pattern/ex13.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+  for(int i=i;i<=5;i++){
+    for(int j=1;j<=i;j++){
+      printf("%d",i*i);
+    }
+    printf("\n");
+  }
+  return 0;
+}
