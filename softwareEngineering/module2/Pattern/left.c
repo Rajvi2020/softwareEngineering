38332a48ed46dbfd66f1797200927c60886fb767
Rajvi2020 <250327823+Rajvi2020@users.noreply.github.com>
2026-03-02 16:27:22 +0530

    commit

--- softwareEngineering/module2/Pattern/left.c
+++ softwareEngineering/module2/Pattern/left.c
@@ -1,32 +1,16 @@
-// Online C compiler to run C program online
-#include <stdio.h>
+#include<stdio.h>
+int main(){
+ 
 
-int main() {
-  
-
-    // This loop for traverse pyramid from top to bottom
-    for (int i = 1; i <=5; i++)
-    {
-char ch='A';
-        // This loop for printing leading whitespaces
-        for (int space=1; space <= 5-i; space++)
-        {
-            printf(" ");
+    for(int i=5;i>=1;i--){
+          char ch='A';
+        for(int j=1;j<=i;j++){
+            printf(" %c",ch);
+            ch++;
         }
-
-        // This loop for printing * character in each row
-        for (int j = 1; j <= i; j++)
-        {
-            printf("* ");
-           ch++;
-        }
-         
-              
-     
         printf("\n");
-       
     }
-    
+
 
     return 0;
 }
